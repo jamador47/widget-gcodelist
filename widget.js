b77a13de99302a21c7f08bc1d7803e50e839438f
Erick Zelaya <ealexander.zm@gmail.com>
2017-05-08 22:34:19 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -1589,12 +1589,7 @@ cpdefine("inline:com-chilipeppr-widget-gcode", ["chilipeppr_ready", "waypoints",
             this.onPlayNextLine();
         },
         onPause: function (event, isFromM6, isFromCpPause) {
-            if(localStorage.getItem('index')){
-                console.log("incrementar");
-                var index = localStorage.getItem('index');
-                index++;
-                localStorage.setItem('index', index);
-            }
+            
             if (this.isPaused) {
                 console.log("onPause. was previously paused by user, unpausing");
                 this.isPaused = false;
